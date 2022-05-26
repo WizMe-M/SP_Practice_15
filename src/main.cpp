613f2a5747f58cb281ba274dddf30f517c754e19
WizMe-M <timkin.moxim@mail.ru>
2022-05-26 12:16:36 +0300

    works without buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -8,154 +8,102 @@
 #define DATA_PIN 8
 #define BUZZER_PIN 3
 
-#define RX_PIN 0
-#define TX_PIN 1
+#define BUZZER_ON 0
+#define BUZZER_OFF 255
 
-#define SLEEP "SLEEP"
-#define START "START"
-#define PAUSE "PAUSE"
-#define STOP "STOP"
-
-String input;
+SoftwareSerial bluetooth(0, 1); // rx = 0, tx = 1 !!!
 OneButton btn(A2);
-
-Event timer;
-bool alarming = false;
-
-//общее количество тиков таймера (количество секунд)
-unsigned int sleepTime;
-
-unsigned int currentSleepTime;
+Timer displayTimer;
 
 //коды для цифр от 0 до 9 под соответствующими индексами
 const int number_codes[] = {~B11111100, ~B01100000, ~B11011010, ~B11110010, ~B01100110,
                             ~B10110110, ~B10111110, ~B11100000, ~B11111110, ~B11110110};
-
 // коды для разрядов числа, начиная с разряда единиц и заканчивая разрядом тысяч
-const int digit_codes[] = {0xF8, 0xF4, 0xF2, 0xF1};
-
+const int digit_codes[] = {0xF1, 0xF2, 0xF4, 0xF8 };
 //цифры для вывода
 uint8_t digits[4];
 
-void stopAlarm()
-{
-  alarming = false;
-  analogWrite(BUZZER_PIN, 255);
-}
-
-void separateNumber(int num)
-{
-  for (uint8_t i = 3; i >= 0; i--)
-  {
-    digits[i] = num % 10;
-    num /= 10;
-  }
-
-  for (int i = 3; i > 0; i--)
-  {
-    if (digits[i] == 0)
-    {
-      digits[i] = 10;
-    }
-    else
-    {
-      break;
-    }
-  }
-}
-
-void convertToInt(int mins, int secs)
-{
-  sleepTime = mins * 60 + secs;
-  currentSleepTime = sleepTime;
-  separateNumber(currentSleepTime);
-}
-
-void countdown()
-{
-  separateNumber(currentSleepTime);
-  currentSleepTime--;
-}
-
-void setup()
-{
-  Serial.begin(9600);
-
-  pinMode(LATCH_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(BUZZER_PIN, OUTPUT);
-  Serial.println("Hi, I am Arduino Timkin");
-
-  convertToInt(01, 00);
-  timer.initEvery(1000, countdown, sleepTime);
-
-  btn.attachClick(stopAlarm);
-}
+int maxSeconds;
+int currentSeconds;
 
 bool isCorrect(int time)
 {
   return time >= 0 && time < 60;
 }
 
+void separateTime()
+{
+  //из 142 секунд
+  uint8_t minutes = currentSeconds / 60;
+  uint8_t seconds = currentSeconds % 60;
+  //в 02:22
+  digits[0] = minutes / 10;
+  digits[1] = minutes % 10;
+  digits[2] = seconds / 10;
+  digits[3] = seconds % 10;
+}
+
+void setTime(int minutes, int seconds)
+{
+  maxSeconds = minutes * 60 + seconds;
+  bluetooth.println((String)"Timer set on " + maxSeconds + (String)" seconds");
+  currentSeconds = maxSeconds;
+}
+
+void countdown()
+{
+  currentSeconds--;
+  bluetooth.println((String)"Timer updated: " + currentSeconds);
+  separateTime();
+}
+
 void handleInput(String s)
 {
-  if (s.startsWith(SLEEP) && s.length() == 10)
+  if (s.startsWith("SLEEP") && s.length() == 10)
   {
     int mins = s.substring(5, 7).toInt();
     int secs = s.substring(8, 10).toInt();
     if (!isCorrect(mins))
     {
-      Serial.println("Minutes should be in range from 00 to 59");
+      bluetooth.println("Minutes should be in range from 00 to 59");
     }
 
     if (!isCorrect(secs))
     {
-      Serial.println("Seconds should be in range from 00 to 59");
+      bluetooth.println("Seconds should be in range from 00 to 59");
     }
 
     if (!isCorrect(mins) || !isCorrect(secs))
       return;
 
-    convertToInt(mins, secs);
-    Serial.println((String) "i sleep... " + mins + (String) ":" + secs);
-    timer.initEvery(1000, countdown, sleepTime);
+    bluetooth.println("i sleep... ");
+    setTime(mins, secs);
+    separateTime();
+    displayTimer.init(1000, countdown, maxSeconds);
   }
-  else if (s == START)
+  else if (s == "START")
   {
-    Serial.println("timer tries to start");
-    timer.resume();
+    bluetooth.println("timer tries to start");
+    displayTimer.start();
   }
-  else if (s == PAUSE)
+  else if (s == "PAUSE")
   {
-    Serial.println("timer tries to pause");
-    timer.pause();
+    bluetooth.println("timer tries to pause");
+    displayTimer.pause();
   }
-  else if (s == STOP)
+  else if (s == "STOP")
   {
-    Serial.println("timer tries to stop");
-    timer.stop();
+    bluetooth.println("timer tries to stop");
+    displayTimer.stop();
+    currentSeconds = maxSeconds;
   }
   else
   {
-    Serial.println("Some unhandled input...");
-  }
-  Serial.println();
-}
-
-void alarm()
-{
-  if (alarming)
-  {
-    analogWrite(BUZZER_PIN, 0);
-  }
-  else
-  {
-    analogWrite(BUZZER_PIN, 255);
+    bluetooth.println("Some unhandled input...");
   }
 }
 
-void displayTime()
+void refreshIndicator()
 {
   for (int i = 0; i < 4; i++)
   {
@@ -166,17 +114,32 @@ void displayTime()
   }
 }
 
+void setup()
+{
+  bluetooth.begin(9600);
+  bluetooth.flush();
+  //ожидаем подтверждения подключения от терминала
+  while (!bluetooth.available()) ;
+  //считываем данные, чтобы следующий input не был битым
+  bluetooth.readString();
+  bluetooth.println("Hi, I am Arduino Timkin");
+
+  //настраиваем семисегментник на вывод
+  pinMode(LATCH_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
+}
+
 void loop()
 {
-  timer.update();
-  btn.tick();
-  if (Serial.available())
+  if (bluetooth.available())
   {
-    input = Serial.readString();
+    String input = bluetooth.readString();
     input.trim();
-    Serial.println((String) "Got input: `" + input + "`");
+    bluetooth.println((String) "# Got input: `" + input + "`");
     handleInput(input);
   }
-  displayTime();
-  alarm();
+
+  displayTimer.update();
+  refreshIndicator();
 }
